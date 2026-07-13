cdf5bb6493078ecacd9d27290bba2a0e8a133af4
Hanna Kovalenko <annet-ko@i.ua>
2026-07-13 18:02:25 -0500

    events HTTP, Express

---
+++ controllers/timeController.js
@@ -0,0 +1,17 @@
+//route handler functions
+function getTime(req, res) {
+  res.status(200).json({
+    time: new Date().toString(),
+  });
+}
+
+function echoBody(req, res) {
+  res.status(200).json({
+    weReceived: req.body,
+  });
+}
+
+module.exports = {
+  getTime,
+  echoBody,
+};
