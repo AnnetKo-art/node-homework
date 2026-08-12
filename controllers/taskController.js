f06bce523f2a50c5f1a90d879b46287c44cf6fdb
Hanna Kovalenko <annet-ko@i.ua>
2026-08-11 21:16:11 -0500

    Complete Assignment 6: Prisma ORM Integration

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,130 +1,164 @@
 const { taskSchema, patchTaskSchema } = require("../validation/taskSchema");
-const pool = require("../db/pg-pool");
+const prisma = require("../db/prisma");
 
 //CREATE
-exports.create = async (req, res,next) => {
+exports.create = async (req, res, next) => {
   if (!req.body) req.body = {};
   const { error, value } = taskSchema.validate(req.body, { abortEarly: false });
   if (error) {
-  return res.status(400).json({
-    message: error.message,
-  });
-}  
-  const task  = await pool.query(`INSERT INTO tasks (title, is_completed, user_id) 
-  VALUES ( $1, $2, $3 ) RETURNING id, title, is_completed`,
- // [value.title, value.is_completed, global.user_id]);
- [value.title, value.isCompleted, global.user_id]);
-  return res.status(201).json(task.rows[0]);
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+  try {
+    const task = await prisma.task.create({
+      data: {
+        title: value.title,
+        isCompleted: value.isCompleted,
+        userId: global.user_id,
+      },
+      select: { id: true, title: true, isCompleted: true },
+    });
+
+    return res.status(201).json(task);
+  } catch (err) {
+    return next(err);
+  }
 };
 
 //INDEX
-exports.index = async (req, res,next) => {
-   const tasks = await pool.query(
-        "SELECT id, title, is_completed FROM tasks WHERE user_id = $1",
-        [global.user_id]
-    );
-if (tasks.rows.length === 0) {
-  return res.status(404).json({
-    message: "The requested task is not found for the current user"
+exports.index = async (req, res, next) => {
+  const tasks = await prisma.task.findMany({
+    where: {
+      userId: global.user_id, // only the tasks for this user
+    },
+    select: { title: true, isCompleted: true, id: true },
   });
-}
-  return res.status(200).json(tasks.rows);
+  if (tasks.length === 0) {
+    return res.status(404).json({
+      message: "The requested task is not found for the current user",
+    });
+  }
+  return res.status(200).json(tasks); //The word Return can be taken away here
 };
- 
+
 //SHOW
-exports.show = async (req, res,next) =>  {
-const taskId = parseInt(req.params?.id);
-if(Number.isNaN(taskId))
-{
-return res.status(400).json({
-   message: "Request data is invalid"
-});
-}
-const task = await pool.query(
-        "SELECT id, title, is_completed FROM tasks WHERE id = $1 AND user_id = $2",
-        [taskId, global.user_id]
-    );
+exports.show = async (req, res, next) => {
+  const taskId = parseInt(req.params?.id);
+  if (Number.isNaN(taskId)) {
+    return res.status(400).json({
+      message: "Request data is invalid",
+    });
+  }
+  try {
+    const task = await prisma.task.findUnique({
+      where: {
+        id_userId: {
+          id: taskId,
+          userId: global.user_id,
+        },
+      },
 
-if(task.rowCount === 0) {
-        return res.status(404).json({
-        message: "The requested task is not found for the current user"
-  });
-}
-return res.status(200).json(task.rows[0]);
+      select: {
+        id: true,
+        title: true,
+        isCompleted: true,
+      },
+    });
 
+    if (!task) {
+      return res.status(404).json({
+        message: "The requested task is not found for the current user",
+      });
+    }
+    return res.status(200).json(task);
+  } catch (err) {
+    if (err.code === "P2025") {
+      return res.status(404).json({
+        message: "The requested task is not found for the current user",
+      });
+    }
+    return next(err);
+  }
 };
 
 //UPDATE
-exports.update = async (req, res,next) =>  {
-if (!req.body) req.body = {};
-  const { error, value } = patchTaskSchema.validate(req.body, { abortEarly: false });
+exports.update = async (req, res, next) => {
+  const taskId = parseInt(req.params?.id);
+  if (Number.isNaN(taskId)) {
+    return res.status(400).json({
+      message: "Request data is invalid",
+    });
+  }
+  if (!req.body) req.body = {};
+  const { error, value } = patchTaskSchema.validate(req.body, {
+    abortEarly: false,
+  });
   if (error) {
-  return res.status(400).json({
-    message: error.message,
-  });
-}
-const taskId = parseInt(req.params?.id);
-if(Number.isNaN(taskId))
-{
-return res.status(400).json({
-   message: "Request data is invalid"
-});
-}
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
 
-if (Object.keys(value).length === 0) {
-        return res.status(400).json({
-        message: "No fields provided to update"
-  });
-}
-
-    let keys = Object.keys(value);
-    keys = keys.map((key) => key === "isCompleted" ? "is_completed" : key);
-    
-    const setClauses = keys.map((key, i) => `${key} = $${i + 1}`).join(", ");
-    const idParm = `$${keys.length + 1}`;
-    const userParm = `$${keys.length + 2}`;
-    const updatedTask = await pool.query(
-        `UPDATE tasks SET ${setClauses} 
-         WHERE id = ${idParm} AND user_id = ${userParm} 
-         RETURNING id, title, is_completed`,
-        [...Object.values(value), taskId, global.user_id]
-    );
-    if (updatedTask.rowCount === 0) {
-        return res.status(404).json({
-            message: "The requested task is not found for the current user"
-        });
+  if (Object.keys(value).length === 0) {
+    return res.status(400).json({
+      message: "No fields provided to update",
+    });
+  }
+  let updatedTask;
+  try {
+    updatedTask = await prisma.task.update({
+      data: value,
+      where: {
+        id: taskId,
+        userId: global.user_id,
+      },
+      select: {
+        title: true,
+        isCompleted: true,
+        id: true,
+      },
+    });
+  } catch (err) {
+    if (err.code === "P2025") {
+      return res.status(404).json({
+        message: "The requested task is not found for the current user",
+      });
     }
-return res.status(200).json(updatedTask.rows[0]);
 
-}
+    return next(err);
+  }
+  return res.status(200).json(updatedTask);
+};
 
 //DELETE TASK
-exports.deleteTask=async(req,res, next)=>{
-const taskId = parseInt(req.params?.id);
-if(Number.isNaN(taskId))
-{
-return res.status(400).json({
-   message: "Request data is invalid"
-});
-}
-
-const deletedTask = await pool.query(
-        `DELETE FROM tasks 
-         WHERE id = $1 AND user_id = $2 
-         RETURNING id, title, is_completed`,
-        [taskId, global.user_id]
-    );
-
-if(deletedTask.rowCount===0) {
-        return res.status(404).json({
-        message: "The requested task is not found for the current user"
-  });
-}
-return res.status(200).json(deletedTask.rows[0]);
-
-}
-
-
-
-
-
+exports.deleteTask = async (req, res, next) => {
+  const taskId = parseInt(req.params?.id);
+  if (Number.isNaN(taskId)) {
+    return res.status(400).json({
+      message: "Request data is invalid",
+    });
+  }
+  let deletedTask;
+  try {
+    deletedTask = await prisma.task.delete({
+      where: {
+        id: taskId,
+        userId: global.user_id,
+      },
+      select: {
+        id: true,
+        title: true,
+        isCompleted: true,
+      },
+    });
+  } catch (err) {
+    if (err.code === "P2025") {
+      return res.status(404).json({
+        message: "The requested task is not found for the current user",
+      });
+    }
+    return next(err);
+  }
+  return res.status(200).json(deletedTask);
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const { userSchema } = require("../validation/userSchema");
 const crypto = require("crypto");
 const util = require("util");
 const scrypt = util.promisify(crypto.scrypt);
-const pool = require("../db/pg-pool");
+const prisma = require("../db/prisma");
 
 
 async function hashPassword(password) {
@@ -27,53 +27,56 @@ exports.register=async(req,res,next)=>{
     details: error.details,
   });
 }   
+value.hashedPassword = await hashPassword(value.password);
+delete value.password; // delete the raw password
+const { name, email, hashedPassword } = value;
 let user = null;
-value.hashed_password = await hashPassword(value.password);
 try {
-        user = await pool.query(
-            `INSERT INTO users (email, name, hashed_password) 
-             VALUES ($1, $2, $3) RETURNING id, email, name`,
-            [value.email, value.name, value.hashed_password]
-        );
-    } catch (e) {
-        if (e.code === "23505") {
+        user = await prisma.user.create({
+    data: { name, email, hashedPassword },
+    select: { name: true, email: true, id: true} // specify the column values to return
+  });
+    } catch (err) {
+        if (err.name === "PrismaClientKnownRequestError" && err.code === "P2002") {
             return res.status(400).json({
-                //error: "This email is already registered."
                 message:"This email is already registered."
             });
         }
-        return next(e); 
+        return next(err); 
     }
-global.user_id = user.rows[0].id;
+global.user_id = user.id;
     return res.status(201).json({
-   name: user.rows[0].name,
-   email: user.rows[0].email
+   name: user.name,
+   email: user.email
 });
 };
 
 //LOGON 
 exports.logon=async(req, res,next)=>{
-    const email = req.body.email;
+    //const email = req.body.email;
+    const email=req.body.email.toLowerCase();
     const password = req.body.password;
-    const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    const user = await prisma.user.findUnique({ 
+        where: { email }
+    });
 
-    if (result.rows.length === 0) {
+    if (!user) {
         return res.status(401).json({
             error: "Invalid email or password"
         });
     }
     
-    const goodCredentials = await comparePassword(password, result.rows[0].hashed_password);
+    const goodCredentials = await comparePassword(password,user.hashedPassword);
 
     if (!goodCredentials) {
         return res.status(401).json({
             error: "Invalid email or password"
         });
     }
-     global.user_id = result.rows[0].id;
+    global.user_id = user.id;
         return res.status(200).json({
-            name: result.rows[0].name,
-            email: result.rows[0].email
+            name: user.name,
+            email: user.email
         });    
 }
 
